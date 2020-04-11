f3400251245eb1a11c5b820290d1e4ae67393fe0
fgoesfer <herfabiano@gmail.com>
2020-04-11 18:30:08 -0300

    Implementation of list add

--- todo/main/views.py
+++ todo/main/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from .models import Todo
+
 
 def home(request):
     return render(request, 'main/index.html')
 
+@csrf_exempt
+def add_to_do(request):
+    current_data = timezone.now()
+    content = request.POST['content']
+    #Todo.added_date = current_data
+    #Todo.text = content
+    aux = Todo.objects.create(added_date=current_data, text=content)
+    
+    return HttpResponseRedirect("/")
